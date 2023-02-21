e8fe41101bd7d45bd7a8b77d0f7d4d163f095394
gabriel <gaamdal557@gmail.com>
2023-02-20 22:36:04 -0300

    configurations e beans

--- vendas/src/main/java/org/gabriel/Main.java
+++ vendas/src/main/java/org/gabriel/Main.java
@@ -1,5 +1,7 @@
 package org.gabriel;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Main {
+
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
+
     @GetMapping("/hello")
     public String helloWord(){
-        return "Hello Word";
+        return applicationName;
     }
     public static void main(String[] args) {
        SpringApplication.run(Main.class, args);
