1fc65c51ba6ba92839b55092db97204c83a4af0c
gabriel <gaamdal557@gmail.com>
2023-02-23 15:01:34 -0300

    mapeando a entidade produto

--- vendas/src/main/java/org/gabriel/domain/entity/Cliente.java
+++ vendas/src/main/java/org/gabriel/domain/entity/Cliente.java
@@ -9,6 +9,7 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Integer id;
+
     @Column(name = "nome", length = 100)
     private String nome;
 
