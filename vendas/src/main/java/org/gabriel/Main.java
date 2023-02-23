8215f9f51f49d44cefa8dd9da1b131bff8f38562
gabriel <gaamdal557@gmail.com>
2023-02-23 09:32:00 -0300

    salvando e recuperando clentes com jdbc

--- vendas/src/main/java/org/gabriel/Main.java
+++ vendas/src/main/java/org/gabriel/Main.java
@@ -1,14 +1,31 @@
 package org.gabriel;
 
 
+import org.gabriel.domain.entity.Cliente;
+import org.gabriel.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class Main {
 
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+             clientes.salvar( new Cliente("Gabriel"));
+             clientes.salvar( new Cliente("Outro CLiente"));
+
+             List<Cliente> todosClientes =  clientes.obterTodos();
+             todosClientes.forEach(System.out::println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
