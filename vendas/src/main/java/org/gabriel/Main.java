f7c3f5232a28d9d08424dddde29bcb1b54bfe0fb
gabriel <gaamdal557@gmail.com>
2023-02-24 13:19:34 -0300

    requisicao get e response entity

--- vendas/src/main/java/org/gabriel/Main.java
+++ vendas/src/main/java/org/gabriel/Main.java
@@ -1,13 +1,25 @@
 package org.gabriel;
 
 
+import org.gabriel.domain.entity.Cliente;
+import org.gabriel.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Main {
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
+        return args -> {
+            Cliente cliente = new Cliente(null,"fulano");
+            clientes.save(cliente);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
--- vendas/src/main/java/org/gabriel/rest/controller/ClienteController.java
+++ vendas/src/main/java/org/gabriel/rest/controller/ClienteController.java
@@ -1,22 +1,32 @@
 package org.gabriel.rest.controller;
 
 import org.gabriel.domain.entity.Cliente;
+import org.gabriel.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(
-            value = {"/api/clientes/hello/{nome}", "/api/hello"},
-            method = RequestMethod.POST,
-            consumes = {"application/json", "application/xml"},
-            produces = {"application/json", "application/xml"}
-    )
+    @Autowired
+    private Clientes clientes;
+
+
+    @GetMapping("/{id}")
     @ResponseBody
-    public Cliente helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente) {
-        return String.format("Hello %s ", nomeCliente);
+    public ResponseEntity getClienteById(@PathVariable Integer id ){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            return ResponseEntity.ok( cliente.get() );
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 }
