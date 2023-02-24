66722583f3675a09274eb1d430f60d87f4ab6fc6
gabriel <gaamdal557@gmail.com>
2023-02-24 12:53:34 -0300

    request mapping

--- vendas/src/main/java/org/gabriel/rest/controller/ClienteController.java
+++ vendas/src/main/java/org/gabriel/rest/controller/ClienteController.java
@@ -1,18 +1,22 @@
 package org.gabriel.rest.controller;
 
+import org.gabriel.domain.entity.Cliente;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(
+            value = {"/api/clientes/hello/{nome}", "/api/hello"},
+            method = RequestMethod.POST,
+            consumes = {"application/json", "application/xml"},
+            produces = {"application/json", "application/xml"}
+    )
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente ){
-        return String.format("Hello %s ", nomeCliente );
+    public Cliente helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente) {
+        return String.format("Hello %s ", nomeCliente);
     }
+
 }
