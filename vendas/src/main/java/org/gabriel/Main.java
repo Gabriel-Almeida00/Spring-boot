a3b15f686aeadedcaec81e5fa6b8c41723bfc479
gabriel <gaamdal557@gmail.com>
2023-02-24 12:44:50 -0300

    controller de clientes

--- vendas/src/main/java/org/gabriel/Main.java
+++ vendas/src/main/java/org/gabriel/Main.java
@@ -1,48 +1,13 @@
 package org.gabriel;
 
 
-import org.gabriel.domain.entity.Cliente;
-import org.gabriel.domain.entity.Pedido;
-import org.gabriel.domain.repository.Clientes;
-import org.gabriel.domain.repository.Pedidos;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class Main {
-
-    @Bean
-    public CommandLineRunner init(
-            @Autowired Clientes clientes,
-            @Autowired Pedidos pedidos
-    ) {
-        return args -> {
-            System.out.println("Salvando clientes");
-            Cliente fulano = new Cliente("Fulano");
-            clientes.save(fulano);
-
-            Pedido p = new Pedido();
-            p.setCliente(fulano);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-            pedidos.save(p);
-
-            Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
-            System.out.println(cliente);
-            System.out.println(cliente.getPedidos());
-
-        };
-    }
-
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
