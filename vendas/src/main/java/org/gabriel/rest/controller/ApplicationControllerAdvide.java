2f39d291888fbe777052a4226c26643406859198
gabriel <gaamdal557@gmail.com>
2023-02-25 13:51:49 -0300

    bean validation

--- vendas/src/main/java/org/gabriel/rest/controller/ApplicationControllerAdvide.java
+++ vendas/src/main/java/org/gabriel/rest/controller/ApplicationControllerAdvide.java
@@ -3,9 +3,13 @@ package org.gabriel.rest.controller;
 import org.gabriel.exception.PedidoNaoEncontradoExecption;
 import org.gabriel.exception.RegrasNegocioException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvide {
@@ -19,7 +23,17 @@ public class ApplicationControllerAdvide {
 
     @ExceptionHandler(PedidoNaoEncontradoExecption.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiErrors handlePedidoNotFoundException(PedidoNaoEncontradoExecption ex){
+    public ApiErrors handlePedidoNotFoundException(PedidoNaoEncontradoExecption ex) {
         return new ApiErrors(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleMethodNotValidException(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map(erro -> erro.getDefaultMessage())
+                .collect(Collectors.toList());
+        return  new ApiErrors(errors);
     }
 }
