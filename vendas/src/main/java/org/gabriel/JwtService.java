f10be6078c9d34651b0d740cc3d4ac78562ce015
gabriel <gaamdal557@gmail.com>
2023-02-26 18:41:01 -0300

    decodficando token

--- vendas/src/main/java/org/gabriel/JwtService.java
+++ vendas/src/main/java/org/gabriel/JwtService.java
@@ -1,5 +1,7 @@
 package org.gabriel;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.gabriel.domain.entity.Usuario;
@@ -8,10 +10,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
 
+import java.awt.peer.RobotPeer;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.HashMap;
 
 @Service
 public class JwtService {
@@ -27,6 +31,7 @@ public class JwtService {
         LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
         Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
         Date date = Date.from(instant);
+
         return Jwts
                 .builder()
                 .setSubject(usuario.getLogin())
@@ -36,11 +41,42 @@ public class JwtService {
 
     }
 
+    private Claims obterClamis( String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido (String token){
+        try{
+            Claims claims = obterClamis(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data =
+                    dataExpiracao.toInstant()
+                            .atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(data);
+        }catch (Exception ex){
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException{
+        return (String) obterClamis(token).getSubject();
+    }
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Main.class);
         JwtService service  = context.getBean(JwtService.class);
         Usuario usuario  = Usuario .builder().login("fulano").build();
         String token = service.gerarToken(usuario);
         System.out.println(token);
+
+        boolean isTokenValido = service.tokenValido(token);
+        System.out.println("o token está valido " + isTokenValido);
+
+        System.out.println(service.obterLoginUsuario(token));
     }
 }
+
