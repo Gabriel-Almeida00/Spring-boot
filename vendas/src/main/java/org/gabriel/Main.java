6c2c370630d8bdd86c78cf5e563bad0ca10f779b
gabriel <gaamdal557@gmail.com>
2023-02-27 12:49:23 -0300

    gerando war

--- vendas/src/main/java/org/gabriel/Main.java
+++ vendas/src/main/java/org/gabriel/Main.java
@@ -3,11 +3,12 @@ package org.gabriel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
