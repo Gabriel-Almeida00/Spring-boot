fa6eff22ce5ea16f6dab918ed9f0d5849d2398bb
gabriel <gaamdal557@gmail.com>
2023-02-27 15:35:54 -0300

    cidade repository

--- localizacao/localizacao/src/main/java/com/gabriel/localizacao/LocalizacaoApplication.java
+++ localizacao/localizacao/src/main/java/com/gabriel/localizacao/LocalizacaoApplication.java
@@ -1,15 +1,33 @@
 package com.gabriel.localizacao;
 
+import com.gabriel.localizacao.domain.entity.Cidade;
+import com.gabriel.localizacao.domain.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class LocalizacaoApplication implements CommandLineRunner {
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Inicializado");
+		salvarcidade();
+		listarCidade();
+	}
+
+	@Transactional
+	void salvarcidade(){
+		var cidade = new Cidade(1L,"são paulo", 12396372L);
+		cidadeRepository.save(cidade);
+	}
+
+	void listarCidade(){
+		cidadeRepository.findAll().forEach(System.out::println);
 	}
 
 	public static void main(String[] args) {
---
+++ localizacao/localizacao/src/main/java/com/gabriel/localizacao/domain/repository/CidadeRepository.java
@@ -0,0 +1,7 @@
+package com.gabriel.localizacao.domain.repository;
+
+import com.gabriel.localizacao.domain.entity.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+}
